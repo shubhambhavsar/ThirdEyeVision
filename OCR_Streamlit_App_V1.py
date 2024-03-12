0a7ca9f7e319f2fb8699c84c64905f95cb3b46ac
Shubham Bhavsar <bhavsar3798@gmail.com>
2024-03-12 17:59:23 -0400

    test

--- OCR_Streamlit_App_V1.py
+++ OCR_Streamlit_App_V1.py
@@ -22,7 +22,7 @@ def speak(text):
 
 # Replace the relative path to your weight file
 model_path = "best_Model_Roboflow.pt"
-source_path = 0 #r"C:/Users/user/OneDrive - Loyalist College/AIandDS/Term 2/Step_Presentation/Videos/2.mp4"
+source_path = 1 #r"C:/Users/user/OneDrive - Loyalist College/AIandDS/Term 2/Step_Presentation/Videos/2.mp4"
 
 # Initialize a Counter object to store text occurrences
 text_occurrences_global = Counter()
